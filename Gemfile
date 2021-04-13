ce2c3f954b8d7de3f1b25e27b92b35e5e7b6a641
georgegeorgescu2000 <georgegeorgescu2000@gmail.com>
2021-04-13 17:08:13 +0300

    update files for heroku

--- Gemfile
+++ Gemfile
@@ -10,10 +10,12 @@ end
 gem 'rails', '~> 5.1.7'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
-gem 'sqlite3'
+  gem 'sqlite3'
+
 end
 group :production do
   gem 'pg'
+  
 end
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
 
- # get "/users", to: "users#index"
- # get "/users/:id", to: "users#show"
+  resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-resources :users
-#, only: [:new, :create]
 end
